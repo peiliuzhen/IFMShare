3757e8eb7f5526a25c948663f9f000725e4c79d4
peiliuzhen <853415458@qq.com>
2018-08-08 16:37:32 +0800

    first commit

---
+++ IFMShareDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IFMShareDemo
+//
+//  Created by plz on 2017/12/5.
+//  Copyright © 2017年 XiaomaTV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IFMShareDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  IFMShareDemo
+//
+//  Created by plz on 2017/12/5.
+//  Copyright © 2017年 XiaomaTV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
